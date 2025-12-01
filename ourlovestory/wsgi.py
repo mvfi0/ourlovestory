0afccd7cec82ecaa0a12ca93fb7afaa70a9db427
mvfi0 <muhammad.vegard@ui.ac.id>
2025-12-02 00:12:40 +0700

    Fix WSGI app alias, add Whitenoise middleware, and pin Django version

--- ourlovestory/wsgi.py
+++ ourlovestory/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for ourlovestory project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ourlovestory.settings')
 
 application = get_wsgi_application()
+
+# Tambahkan baris ini agar Vercel bisa menemukan aplikasinya
+app = application
