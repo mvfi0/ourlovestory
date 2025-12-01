4d749955b2a51eb109f6529e63b807920eab1ca5
mvfi0 <muhammad.vegard@ui.ac.id>
2025-12-02 00:00:58 +0700

    Fix build script line endings and permissions

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+#!/bin/bash
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "Build Completed!"
