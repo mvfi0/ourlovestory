b5d09506cb5da16f208605b40deffedc44b521d9
mvfi0 <muhammad.vegard@ui.ac.id>
2025-12-02 00:04:47 +0700

    Remove psycopg2 and add error handling to build script

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e  # Hentikan script jika ada command yang gagal
+
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
