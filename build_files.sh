19a24f8ba08a54c9049224ba2347af60e031132e
mvfi0 <muhammad.vegard@ui.ac.id>
2025-12-02 00:06:53 +0700

    Add ensurepip to build script

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,17 @@
 set -e  # Hentikan script jika ada command yang gagal
 
 echo "Building the project..."
+
+# 1. Install PIP secara manual karena Vercel kadang tidak menyediakannya di Python 3.9
+python3.9 -m ensurepip --default-pip
+
+# 2. Update PIP ke versi terbaru (opsional, tapi disarankan)
+python3.9 -m pip install --upgrade pip
+
+# 3. Install dependencies project kamu
 python3.9 -m pip install -r requirements.txt
+
+# 4. Jalankan perintah Django
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "Build Completed!"
